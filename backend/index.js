8f31d874d05685c50b80444788c2c0ed7d31ae8f
Afacehacker <afaceabolade@gmail.com>
2026-02-21 03:16:30 +0100

    Add root route

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ prisma.$connect()
     .catch((err) => console.error('Prisma connection error:', err));
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://godisgood-lilac.vercel.app', 'http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
